9a8831237f042e6042b96bdd54a7333b52c27d8f
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-07-15 13:22:26 +0700

    User login

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import 'dotenv/config'
 
+import authRoutes from './routes/auth'
 import userRoutes from './routes/users'
 
 const app = express()
@@ -12,6 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
+app.use('/api/auth', authRoutes)
 app.use('/api/users', userRoutes)
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
