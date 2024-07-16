25d2f7069f3202ecdbb8467a40155586e6ef5a17
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-07-16 16:08:40 +0700

    Create EditHotel page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ import SignIn from './pages/SignIn'
 import AddHotel from './pages/AddHotel'
 import { useAppContext } from './context/AppContext'
 import MyHotels from './pages/MyHotels'
+import EditHotel from './pages/EditHotel'
 
 const App = () => {
 	const { isLoggedIn } = useAppContext()
@@ -61,6 +62,15 @@ const App = () => {
 							element={
 								<Layout>
 									<AddHotel />
+								</Layout>
+							}
+						/>
+
+						<Route
+							path='/edit-hotel/:hotelId'
+							element={
+								<Layout>
+									<EditHotel />
 								</Layout>
 							}
 						/>
