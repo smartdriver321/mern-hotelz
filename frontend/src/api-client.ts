c4a5e34e8fc60c8b540dc74862d56a62d6242577
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-07-17 08:54:35 +0700

    Search page fetch hotels

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -3,6 +3,20 @@ import { RegisterFormData } from './pages/Register'
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || ''
 
+export type SearchParams = {
+	destination?: string
+	checkIn?: string
+	checkOut?: string
+	adultCount?: string
+	childCount?: string
+	page?: string
+	facilities?: string[]
+	types?: string[]
+	stars?: string[]
+	maxPrice?: string
+	sortOption?: string
+}
+
 export const validateToken = async () => {
 	const response = await fetch(`${API_BASE_URL}/api/auth/validate-token`, {
 		credentials: 'include',
@@ -110,6 +124,37 @@ export const updateMyHotelById = async (hotelFormData: FormData) => {
 
 	if (!response.ok) {
 		throw new Error('Failed to update Hotel')
+	}
+
+	return response.json()
+}
+
+export const searchHotels = async (
+	searchParams: SearchParams
+): Promise<HotelSearchResponse> => {
+	const queryParams = new URLSearchParams()
+
+	queryParams.append('destination', searchParams.destination || '')
+	queryParams.append('checkIn', searchParams.checkIn || '')
+	queryParams.append('checkOut', searchParams.checkOut || '')
+	queryParams.append('adultCount', searchParams.adultCount || '')
+	queryParams.append('childCount', searchParams.childCount || '')
+	queryParams.append('page', searchParams.page || '')
+	queryParams.append('maxPrice', searchParams.maxPrice || '')
+	queryParams.append('sortOption', searchParams.sortOption || '')
+
+	searchParams.types?.forEach((type) => queryParams.append('types', type))
+	searchParams.stars?.forEach((star) => queryParams.append('stars', star))
+	searchParams.facilities?.forEach((facility) =>
+		queryParams.append('facilities', facility)
+	)
+
+	const response = await fetch(
+		`${API_BASE_URL}/api/hotels/search?${queryParams}`
+	)
+
+	if (!response.ok) {
+		throw new Error('Error fetching hotels')
 	}
 
 	return response.json()
--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,9 +1,26 @@
+import { useState } from 'react'
+import { useQuery } from 'react-query'
+
+import * as apiClient from '../api-client'
 import { useSearchContext } from '../context/SearchContext'
 
 const Search = () => {
 	const search = useSearchContext()
-	console.log(search)
 
+	const [page, setPage] = useState<number>(1)
+
+	const searchParams = {
+		destination: search.destination,
+		checkIn: search.checkIn.toISOString(),
+		checkOut: search.checkOut.toISOString(),
+		adultCount: search.adultCount.toString(),
+		childCount: search.childCount.toString(),
+		page: page.toString(),
+	}
+
+	const { data: hotelData } = useQuery(['searchHotels', searchParams], () =>
+		apiClient.searchHotels(searchParams)
+	)
 	return <div>Search</div>
 }
 
