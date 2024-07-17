f88b2b21edfaac1c113c60b13882b728f2af87ee
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-07-17 14:30:21 +0700

    FasilitiesFilter component (Not fixed  yet)

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -7,6 +7,7 @@ import SearchResultsCard from '../components/SearchResultsCard'
 import Pagination from '../components/Pagination'
 import StarRatingFilter from '../components/StarRatingFilter'
 import HotelTypesFilter from '../components/HotelTypesFilter'
+import FacilitiesFilter from '../components/FacilitiesFilter'
 
 const Search = () => {
 	const search = useSearchContext()
@@ -14,6 +15,7 @@ const Search = () => {
 	const [page, setPage] = useState<number>(1)
 	const [selectedStars, setSelectedStars] = useState<string[]>([])
 	const [selectedHotelTypes, setSelectedHotelTypes] = useState<string[]>([])
+	const [selectedFacilities, setSelectedFacilities] = useState<string[]>([])
 
 	const searchParams = {
 		destination: search.destination,
@@ -24,6 +26,7 @@ const Search = () => {
 		page: page.toString(),
 		stars: selectedStars,
 		types: selectedHotelTypes,
+		facilities: selectedFacilities,
 	}
 
 	const { data: hotelData } = useQuery(['searchHotels', searchParams], () =>
@@ -52,6 +55,16 @@ const Search = () => {
 		)
 	}
 
+	const handleFacilityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const facility = event.target.value
+
+		setSelectedFacilities((prevFacilities) =>
+			event.target.checked
+				? [...prevFacilities, facility]
+				: prevFacilities.filter((prevFacility) => prevFacility !== facility)
+		)
+	}
+
 	return (
 		<div className='grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5'>
 			<div className='rounded-lg border border-slate-300 p-5 h-fit sticky top-10'>
@@ -68,6 +81,11 @@ const Search = () => {
 					<HotelTypesFilter
 						selectedHotelTypes={selectedHotelTypes}
 						onChange={handleHotelTypeChange}
+					/>
+
+					<FacilitiesFilter
+						selectedFacilities={selectedFacilities}
+						onChange={handleFacilityChange}
 					/>
 				</div>
 			</div>
