ee6b3cb859dcc0dc864fc09cb217418d74724df2
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-07-17 17:12:38 +0700

    Save search terms to session storage

--- frontend/src/context/SearchContext.tsx
+++ frontend/src/context/SearchContext.tsx
@@ -1,84 +1,3 @@
-// import React, { useContext, useState } from 'react'
-
-// type SearchContext = {
-// 	destination: string
-// 	checkIn: Date
-// 	checkOut: Date
-// 	adultCount: number
-// 	childCount: number
-// 	hotelId: string
-// 	saveSearchValues: (
-// 		destination: string,
-// 		checkIn: Date,
-// 		checkOut: Date,
-// 		adultCount: number,
-// 		childCount: number
-// 	) => void
-// }
-
-// const SearchContext = React.createContext<SearchContext | undefined>(undefined)
-
-// type SearchContextProviderProps = {
-// 	children: React.ReactNode
-// }
-
-// export const SearchContextProvider = ({
-// 	children,
-// }: SearchContextProviderProps) => {
-// 	const [destination, setDestination] = useState<string>('')
-
-// 	const [checkIn, setCheckIn] = useState<Date>(new Date())
-
-// 	const [checkOut, setCheckOut] = useState<Date>(new Date())
-
-// 	const [adultCount, setAdultCount] = useState<number>('1')
-
-// 	const [childCount, setChildCount] = useState<number>('0')
-
-// 	const [hotelId, setHotelId] = useState<string>('')
-
-// 	const saveSearchValues = (
-// 		destination: string,
-// 		checkIn: Date,
-// 		checkOut: Date,
-// 		adultCount: number,
-// 		childCount: number,
-// 		hotelId?: string
-// 	) => {
-// 		setDestination(destination)
-// 		setCheckIn(checkIn)
-// 		setCheckOut(checkOut)
-// 		setAdultCount(adultCount)
-// 		setChildCount(childCount)
-
-// 		if (hotelId) {
-// 			setHotelId(hotelId)
-// 		}
-// 	}
-
-// 	return (
-// 		<SearchContext.Provider
-// 			value={{
-// 				destination,
-// 				checkIn,
-// 				checkOut,
-// 				adultCount,
-// 				childCount,
-// 				hotelId,
-// 				saveSearchValues,
-// 			}}
-// 		>
-// 			{children}
-// 		</SearchContext.Provider>
-// 	)
-// }
-
-// export const useSearchContext = () => {
-// 	const context = useContext(SearchContext)
-// 	return context as SearchContext
-// }
-
-////////////////////
 import React, { useContext, useState } from 'react'
 
 type SearchContext = {
@@ -109,20 +28,25 @@ export const SearchContextProvider = ({
 	const [destination, setDestination] = useState<string>(
 		() => sessionStorage.getItem('destination') || ''
 	)
+
 	const [checkIn, setCheckIn] = useState<Date>(
 		() =>
 			new Date(sessionStorage.getItem('checkIn') || new Date().toISOString())
 	)
+
 	const [checkOut, setCheckOut] = useState<Date>(
 		() =>
 			new Date(sessionStorage.getItem('checkOut') || new Date().toISOString())
 	)
+
 	const [adultCount, setAdultCount] = useState<number>(() =>
 		parseInt(sessionStorage.getItem('adultCount') || '1')
 	)
+
 	const [childCount, setChildCount] = useState<number>(() =>
 		parseInt(sessionStorage.getItem('childCount') || '1')
 	)
+
 	const [hotelId, setHotelId] = useState<string>(
 		() => sessionStorage.getItem('hotelID') || ''
 	)
