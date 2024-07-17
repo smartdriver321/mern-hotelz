e657e30a180c6e09bc0e4f3b72fe361a70963ff5
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-07-17 09:45:59 +0700

    SearchResultsCard component

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -3,6 +3,7 @@ import { useQuery } from 'react-query'
 
 import * as apiClient from '../api-client'
 import { useSearchContext } from '../context/SearchContext'
+import SearchResultsCard from '../components/SearchResultsCard'
 
 const Search = () => {
 	const search = useSearchContext()
@@ -21,7 +22,30 @@ const Search = () => {
 	const { data: hotelData } = useQuery(['searchHotels', searchParams], () =>
 		apiClient.searchHotels(searchParams)
 	)
-	return <div>Search</div>
+	return (
+		<div className='grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5'>
+			<div className='rounded-lg border border-slate-300 p-5 h-fit sticky top-10'>
+				<div className='space-y-5'>
+					<h3 className='text-lg font-semibold border-b border-slate-300 pb-5'>
+						Filter by:
+					</h3>
+				</div>
+			</div>
+
+			<div className='flex flex-col gap-5'>
+				<div className='flex justify-between items-center'>
+					<span className='text-xl font-bold'>
+						{hotelData?.pagination.total} Hotels found
+						{search.destination ? ` in ${search.destination}` : ''}
+					</span>
+				</div>
+
+				{hotelData?.data.map((hotel) => (
+					<SearchResultsCard hotel={hotel} />
+				))}
+			</div>
+		</div>
+	)
 }
 
 export default Search
