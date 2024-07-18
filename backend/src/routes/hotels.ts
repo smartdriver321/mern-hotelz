b10734fc982590d049cb53931184f17a2e25ade5
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-07-18 13:21:55 +0700

    Booking API

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -3,7 +3,7 @@ import { param, validationResult } from 'express-validator'
 import Stripe from 'stripe'
 
 import Hotel from '../models/hotel'
-import { HotelSearchResponse } from '../shared/types'
+import { BookingType, HotelSearchResponse } from '../shared/types'
 import verifyToken from '../middleware/auth'
 
 const router = express.Router()
@@ -164,6 +164,58 @@ router.post(
 		}
 
 		res.send(response)
+	}
+)
+
+router.post(
+	'/:hotelId/bookings',
+	verifyToken,
+	async (req: Request, res: Response) => {
+		try {
+			const paymentIntentId = req.body.paymentIntentId
+			const paymentIntent = await stripe.paymentIntents.retrieve(
+				paymentIntentId as string
+			)
+
+			if (!paymentIntent) {
+				return res.status(400).json({ message: 'Payment intent not found' })
+			}
+
+			if (
+				paymentIntent.metadata.hotelId !== req.params.hotelId ||
+				paymentIntent.metadata.userId !== req.userId
+			) {
+				return res.status(400).json({ message: 'Payment intent mismatch' })
+			}
+
+			if (paymentIntent.status !== 'succeeded') {
+				return res.status(400).json({
+					message: `Payment intent not succeeded. Status: ${paymentIntent.status}`,
+				})
+			}
+
+			const newBooking: BookingType = {
+				...req.body,
+				userId: req.userId,
+			}
+
+			const hotel = await Hotel.findOneAndUpdate(
+				{ _id: req.params.hotelId },
+				{
+					$push: { bookings: newBooking },
+				}
+			)
+
+			if (!hotel) {
+				return res.status(400).json({ message: 'Hotel not found' })
+			}
+
+			await hotel.save()
+			res.status(200).send()
+		} catch (error) {
+			console.log(error)
+			res.status(500).json({ message: 'Something went wrong' })
+		}
 	}
 )
 
